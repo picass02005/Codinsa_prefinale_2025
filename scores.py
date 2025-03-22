e679243380c477f54621f85626c39cbbc4ce2926
Bongibault Romain <bongibault.romain@gmail.com>
2025-03-22 14:50:25 +0100

    basic scores

---
+++ scores.py
@@ -0,0 +1,27 @@
+from typing import List, Tuple
+from Parser import Cake
+
+# Heuristique somme des aires des espaces vides au carrés
+def basic_score(grid: List[List[bool]]) -> float:
+    done = []
+    areas = []
+
+    def find_area(i, j):
+        # Si on dépasse les dimensions de la grille, on est déjà passé par là ou on est sur un gâteau, on renvoie 0
+        if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or (i, j) in done or grid[i][j]:
+            return 0
+        done.append((i, j))
+        return 1 + find_area(i + 1, j) + find_area(i - 1, j) + find_area(i, j + 1) + find_area(i, j - 1)
+
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            # Si on est déjà passé par là ou si on est sur un gâteau, on passe
+            if (i, j) in done or grid[i][j]:
+                continue
+
+            areas.append(find_area(i, j))
+
+    return sum(area ** 2 for area in areas)
+
+
+    
