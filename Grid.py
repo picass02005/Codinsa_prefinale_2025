8374ddc7de7291eeb732545af70d9468a20bfdf7
picasso2005 <clementduran0@gmail.com>
2025-03-22 14:55:51 +0100

    Changed func headers

--- Grid.py
+++ Grid.py
@@ -4,6 +4,13 @@ from typing import List
 from Parser import Cake
 
 
+@dataclass
+class BakingCake:
+    cake: Cake
+    ori_x: int
+    ori_y: int
+
+
 class Grid:
     def __init__(self, x: int, y: int):
         self.x: int = x
@@ -14,14 +21,9 @@ class Grid:
         self.grid: List[List[bool]] = [[False for _ in range(x)] for _ in range(y)]
 
     def add_cake(self, ori_x: int, ori_y: int, cake: Cake):
+        # Check grid
+        for x, y in cake.squares:
+            pass
+
+    def remove_cake(self, cake: BakingCake):
         pass
-
-    def remove_cake(self, ori_x: int, ori_y: int, cake: Cake):
-        pass
-
-
-@dataclass
-class BakingCake:
-    cake: Cake
-    ori_x: int
-    ori_y: int
